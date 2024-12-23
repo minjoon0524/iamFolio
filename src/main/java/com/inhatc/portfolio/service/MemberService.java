58f2255674c412aaa2db4c2b2d34a1fa67b572ab
MINJUN YU <minjoon0524@gmail.com>
2024-12-23 21:31:06 +0900

    style:colons

--- src/main/java/com/inhatc/portfolio/service/MemberService.java
+++ src/main/java/com/inhatc/portfolio/service/MemberService.java
@@ -4,7 +4,6 @@ import com.inhatc.portfolio.entity.Member;
 import com.inhatc.portfolio.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
