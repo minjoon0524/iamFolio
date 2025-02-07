a793028ecaa4f7602d3a1a44fb4244cdb5d74437
MINJUN YU <minjoon0524@gmail.com>
2025-02-07 15:58:34 +0900

    feat:BoardRepository 주석 추가

--- src/main/java/com/inhatc/portfolio/repository/BoardRepository.java
+++ src/main/java/com/inhatc/portfolio/repository/BoardRepository.java
@@ -1,9 +1,18 @@
 package com.inhatc.portfolio.repository;
 
+import com.inhatc.portfolio.dto.BoardResponseDto;
 import com.inhatc.portfolio.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query(value = "select board " +
+            "from Board board " +
+            "where board.title = :keyword OR board.content = :keyword")
+    List<Board> findByTitleOrContent(@Param("keyword") String keyword);
 }
